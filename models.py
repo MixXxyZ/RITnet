f007828a8aab96e330c7c7c8887c812d5b51054f
MixXxyZ <34738580+MixXxyZ@users.noreply.github.com>
2020-09-17 18:06:32 +0700

    add mobilenet_light

--- models.py
+++ models.py
@@ -11,9 +11,11 @@ from densenet import DenseNet2D
 from mixnet import MixNet
 from eyemms import EyeMMS
 from minenet import MinENet
+from mobilenet_light import MobileNetV2_CS
 model_dict = {}
 
 model_dict['densenet'] = DenseNet2D(dropout=True,prob=0.2)
 model_dict['mixnet'] = MixNet(init_weights=True)
 model_dict['EyeMMS'] = EyeMMS(init_weights=True)
 model_dict['MinENet'] = MinENet(4, encoder_relu=False, decoder_relu=False, init_weights=True)
+model_dict['MobileNet_light'] = MobileNetV2_CS()
